6d3188b11fcb863738b92acb156a62f67250d049
Matthias Wagner <matzewagner@gmail.com>
2018-04-10 07:07:50 -0700

    add alt solutions to eu_1

--- eu/eu_1/src/main.rs
+++ eu/eu_1/src/main.rs
@@ -1,5 +1,10 @@
 // Multiples of 3 and 5
 fn main() {
+    eu_1();
+    eu_2();
+}
+
+fn eu_1() {
     let N = 1000;
     let m_1 = 3;
     let m_2 = 5;
@@ -8,7 +13,7 @@ fn main() {
 }
 
 fn multiples_of_n_and_n(N: u32, m_1: u32, m_2: u32) -> u32 {
-    // Verbose:
+    // brute force:
     // let mut c = 0;
     // for x in 0..N {
     //     match (x % m_1, x % m_2) {
@@ -19,5 +24,32 @@ fn multiples_of_n_and_n(N: u32, m_1: u32, m_2: u32) -> u32 {
     //     }
     // }
     // c
-    (1..N).filter(|&x| x % m_1 == 0 || x % m_2 == 0).sum()
+
+    // alt 0:
+    // (1..N).filter(|&x| x % m_1 == 0 || x % m_2 == 0).sum()
+
+    // alt 1:
+    // for each m_, increment until N and save values in a list.
+    // compare the 2 lists and remove duplicates.
+    // sum
+
+    // alt 2 (arithmetic progressions):
+    // sum of terms as (n_terms/2)(a+last)
+    sum_divisible_by(N, m_1) + sum_divisible_by(N, m_2) - sum_divisible_by(N, (m_1 * m_2))
+}
+
+fn sum_divisible_by(N: u32, m: u32) -> u32 {
+    let target = N - 1;
+    let num_terms = (target / m) as u32;
+    let sum = m * (num_terms * (num_terms + 1)) / 2 as u32;
+    sum
+}
+
+fn eu_2() {
+    let N = 4000000;
+    fib(N);
+}
+
+fn fib(N: u32) -> u32 {
+    0
 }
